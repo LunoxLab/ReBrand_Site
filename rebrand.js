316e6647a8c0016edd9d6e846de1b5f928930139
Adil-Im <adhilimam2004@gmail.com>
2025-07-29 02:37:29 +0530

    feat: Added Search bar/Frontend

--- rebrand.js
+++ rebrand.js
@@ -1,3 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const searchIcon = document.querySelector('.search-icon');
+  const searchInput = document.querySelector('.search-input');
+
+  searchIcon.addEventListener('click', function() {
+    searchInput.classList.toggle('active');
+    if (searchInput.classList.contains('active')) {
+      searchInput.focus(); // Focus the input when it appears
+    }
+  });
+});
+
+
 
 
 let addCart = document.querySelectorAll(".add-to-cart");
