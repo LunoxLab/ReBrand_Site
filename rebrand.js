bb0dcdb5f8f147cb414a88be86c752c8e04b2943
wrix244 <rupamdebnath2003@gmail.com>
2025-08-02 01:23:39 +0530

    updated badge counter and local storage

--- rebrand.js
+++ rebrand.js
@@ -1,22 +1,108 @@
 
 
+
+  // 4. Get current cart from localStorage (or start empty)
+ let cart = JSON.parse(localStorage.getItem("cartItems")) || [];
+ 
+
+ document.addEventListener("DOMContentLoaded", () => {
+  IconBadge(cart.length);
+
 let addCart = document.querySelectorAll(".add-to-cart");
 
+function IconBadge(count){
+  // let icon = document.createElement("p");
+  // icon.classList.add("iconStyle");
+  let badgeHolder = document.querySelector(".badgeHolder");
+  if(!badgeHolder) return;
+  if(count>0){
+    badgeHolder.style.display = "block";
+    badgeHolder.textContent = count;
+
+  }else{
+    badgeHolder.style.display = "none";
+  }
+//   badgeHolder.style.display = "block";
+// badgeHolder.textContent = count;
+
+
+}
+
+
+
+
+
+function showToast(message) {
+  const toast = document.getElementById("toast");
+  toast.textContent = message;
+  toast.classList.add("show");
+
+  // Hide it after 2 seconds
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 2000);
+}
+
+
 addCart.forEach(button=>{
-     
-    button.addEventListener("click",()=>{
+  button.addEventListener("click", function(){
+ // 1. Get parent element using closet (the .product div)
+    let product = button.closest(".products");
+
+    //Get Data from it//
+    let name = product.querySelector(".title").textContent;
+    let price = product.querySelector(".price").textContent;
+    let img = product.querySelector(".img").src;
+
+    let item = {
+      id: Date.now(), 
+      name: name,
+      price:price,
+      img:img
+    };
+
+    cart.push(item);
+   updateCartInStorage();
+   IconBadge(cart.length);
+      
 
    
 
-    let span = document.createElement("span");
-    span.classList.add("add-cart-msg");
-    span.textContent = "Added to Cart";
-     let productCard = button.closest(".product-card");
-    
-    if (productCard) {
-      productCard.appendChild(span);
+    // If already added, redirect to cart
+    if(button.classList.contains("Added")){
+      window.location.href="\cart.html";
+      return;
     }
+
+     // Add item to cart logic here...
+     button.textContent = "Go To Cart";
+     button.classList.add("Added");
+     updateCartInStorage();
+
+
+    
    
-     });
+    
+
+    //Show pop out msg
+    showToast("Item Added to Cart Successfully!");
+    //count Icon Badge
+    
+  });
+});
+
+// Number badge count along with cart icon to see increment when adding items to cart//
+
+
+
+//On Clicking Location
+document.getElementById("cart-icon").addEventListener("click",()=>{
+window.location.href = "/cart.html";
+
+});
+
+function updateCartInStorage(){
+     localStorage.setItem("cartItems", JSON.stringify(cart))
+    }
 
 });
